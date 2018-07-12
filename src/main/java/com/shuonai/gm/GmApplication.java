2b85970e708b048aaf7fa50f9114484597a6847f
DestinyXsl <519965081@qq.com>
2018-07-13 01:24:34 +0800

    add:初始化项目

---
+++ src/main/java/com/shuonai/gm/GmApplication.java
@@ -0,0 +1,12 @@
+package com.shuonai.gm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmApplication.class, args);
+	}
+}
